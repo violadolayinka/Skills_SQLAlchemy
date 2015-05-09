b98e3c68a9b013707db491cbb0c115cc4102e176
Viola D. Olayinka <violadolayinka@gmail.com>
2015-05-09 11:37:06 -0700

    Startign homework assignemt for Skill Assessment

---
+++ model.py
@@ -0,0 +1,50 @@
+"""Models and database functions for Ratings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the SQLite database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Part 1: Compose ORM
+
+class Model(db.Model):
+
+    __tablename__ = "models"
+    pass
+
+
+class Brand(db.Model):
+
+    __tablename__ = "brands"
+    pass
+
+# End Part 1
+##############################################################################
+# Helper functions
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auto.db'
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ query.py
@@ -0,0 +1,35 @@
+# Note: this file will not run. It is only for recording answers.
+
+# Part 2: Write queries
+
+# Get the brand with the **id** of 8.
+
+# Get all models with the **name** Corvette and the **brand_name** Chevrolet.
+
+# Get all models that are older than 1960.
+
+# Get all brands that were founded after 1920.
+
+# Get all models with names that begin with "Cor".
+
+# Get all brands with that were founded in 1903 and that are not yet discontinued.
+
+# Get all brands with that are either discontinued or founded before 1950.
+
+# Get any model whose brand_name is not Chevrolet.
+
+
+# Part 2.5: Advanced and Optional
+def search_brands_by_name(mystr):
+    pass
+
+
+def get_models_between(start_year, end_year):
+    pass
+
+# Part 3: Discussion Questions
+
+# 1. What is the returned value and datatype of ``Brand.query.filter_by(name='Ford')``?
+
+# 2. In your own words, what is an association table, and what *type* of relationship 
+# does an association table manage?
